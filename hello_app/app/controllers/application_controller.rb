fc128d723bd2dd204cd018ba1fb3c4a8bd15a2cd
ngajugodwin <ngajugodwin@yahoo.com>
2018-05-23 05:15:41 +0000

    Improve the README file

--- hello_app/app/controllers/application_controller.rb
+++ hello_app/app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Hola, mundo!"
+    render html: "Hello World!"
   end
   
   def goodbye
-    render html: "Goodbye, world!"
+    render html: "Goodby World!"
   end
 end
